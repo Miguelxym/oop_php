fe78170bd571212eff94b3a110e1a3ce5a5bf03f
Miguel Sampaio <miguelsampaio149@gmail.com>
2020-09-06 15:56:42 +0100

    Visibility And Inheritance

--- includes/newClass.inc.php
+++ includes/newClass.inc.php
@@ -1,16 +1,59 @@
 <?php
 class Person
 {
-    private $first_name = '';
-    private $last_name = '';
+    protected $first_name = 'A';
+    protected $last_name = 'V';
     private $age = '';
+
+    public $public = 'public</br>';
+    protected $protected = 'protected</br>';
+    private $private = 'private</br>';
+
+    public function echoProps()
+    {
+        echo "$this->public</br>";
+        echo "$this->protected</br>";
+        echo "$this->private</br>";
+    }
+
+    public function fn_public(){
+        echo "public method</br>";
+    }
+
+    protected function fn_protected(){
+        echo "public method</br>";
+    }
+
+    private function fn_private(){
+        echo "public method</br>";
+    }
+
 }
 
-class Dog
+class Dog extends Person
 {
-    private function echoOwner()
+    public function echoOwner()
     {
-        $var = "Owner: ";
+        $var = "Owner: $this->first_name $this->last_name</br>";
         return $var;
+    }
+
+    public function echoProps2()
+    {
+        echo "$this->public</br>";
+        echo "$this->protected</br>";
+        //secho "$this->private</br>";
+    }
+
+    public function fn_public_ex(){
+        $this->fn_public();
+    }
+
+    protected function fn_protected_ex(){
+        $this->fn_protected();
+    }
+
+    private function fn_private_ex(){
+        $this->fn_private();
     }
 }
--- index.php
+++ index.php
@@ -7,7 +7,34 @@
 </head>
 <body>
     <?php
-        include 'classes/newClass.inc.php';
+        include 'includes/newClass.inc.php';
+        $person = new Person();
+        $dog = new Dog();
+
+        echo "Person props:</br>";
+        echo $person->public;
+        //echo $person->protected;
+        //echo $person->private;
+
+        echo $person->echoProps();
+
+        echo "Dog method w/ props:</br>";
+        echo $dog->echoProps2();
+
+        echo "Person methods:</br>";
+        echo $person->fn_public();
+        //echo $person->fn_protected();
+        //echo $person->fn_private();
+
+        echo "Dog methods 1:</br>";
+        echo $dog->fn_public();
+        //echo $dog->fn_protected();
+        //echo $dog->fn_private();
+
+        echo "Dog methods 2:</br>";
+        echo $dog->fn_public_ex();
+        //echo $dog->fn_protected_ex();
+        //echo $dog->fn_private_ex();
     ?>
     
 </body>
